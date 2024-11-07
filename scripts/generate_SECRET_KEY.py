a5fda45ebb7978164404fced264fee01d7f68686
geekpnx <t.r.n.c@trncthll.local>
2024-11-07 16:30:35 +0100

    revised README

--- scripts/generate_SECRET_KEY.py
+++ scripts/generate_SECRET_KEY.py
@@ -2,4 +2,4 @@ import secrets
 secretkey = secrets.token_urlsafe(50)
 print("\nWe generating Django SECRET_KEY for you")
 print(f"\nYour Django SECRET_KEY is: \033[94m{secretkey}\033[0m")
-print(f"\nPlease added this SECRET_KEY into your '.env' file")
+print(f"\nPlease added this SECRET_KEY into your '.env.prod' file")
